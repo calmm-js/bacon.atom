389d37893148a5494b5bf1e000f68a64f48a76e3
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-01-22 10:37:47 +0200

    Avoid Bacon property access on get.
    This allows `get` to work in cases where it is called while Bacon is
    processing properties.

--- src/bacon.atom.js
+++ src/bacon.atom.js
@@ -1,42 +1,39 @@
 import Bacon from "baconjs"
 import R     from "ramda"
 
-function get() {
-  let result
-  this.take(1).onValue(value => result = value)
-  return result
-}
+function ignore() {}
 
 function set(value) { this.modify(() => value) }
 
-function modifyMap(x2x) { this.parent.modify(R.over(this.mapper, x2x)) }
+function getLens() { return R.view(this.mapper, this.parent.get()) }
+function modifyLens(x2x) { this.parent.modify(R.over(this.mapper, x2x)) }
 
 function lens(l, eq = R.equals) {
   const atom = this.map(R.view(l)).skipDuplicates(eq)
 
   atom.parent = this
   atom.mapper = l
-  atom.modify = modifyMap
-  atom.get = get
+  atom.modify = modifyLens
+  atom.get = getLens
   atom.set = set
   atom.lens = lens
 
   return atom
 }
 
-function modifyBus(x2x) { this.bus.push(x2x) }
-
-function ignore() {}
+function getRoot() { return this.value }
+function modifyRoot(x2x) { this.bus.push(x2x) }
 
 export default (value, eq = R.equals) => {
   const bus = Bacon.Bus()
-  const atom = bus.scan(value, (value, fn) => fn(value)).skipDuplicates(eq)
+  const atom = bus.scan(value, (v, fn) => atom.value = fn(v)).skipDuplicates(eq)
 
   atom.subscribe(ignore)
 
+  atom.value = value
   atom.bus = bus
-  atom.modify = modifyBus
-  atom.get = get
+  atom.modify = modifyRoot
+  atom.get = getRoot
   atom.set = set
   atom.lens = lens
 
